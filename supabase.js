48c9284e067e56a8f2510b20016b49aa5aefb576
kasimbelim28-netizen <kasimbelim28@gmail.com>
2026-02-22 11:52:59 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL="https://xbwgottrnhubmpxhvkrk.supabase.co";
+const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhid2dvdHRybmh1Ym1weGh2a3JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MzI2NzMsImV4cCI6MjA4NzMwODY3M30.EBKwVxdzCtVv9P_S7-IFs1V-NeXRwhuFAL6FUNBEsvk";
+
+const supabase=window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
