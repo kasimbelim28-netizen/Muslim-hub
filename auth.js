c68bf6c8b1db53e456a5f1175b5e9c01b45016d0
kasimbelim28-netizen <kasimbelim28@gmail.com>
2026-02-22 16:49:08 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,23 +1,11 @@
-const CLIENT_ID="1474836154225524787";
-const REDIRECT=window.location.origin;
+function loginWithDiscord() {
+  const CLIENT_ID = "1474836154225524787";
+  const REDIRECT_URI = "https://muslim-hub-cyan.vercel.app/dashboard.html";
 
-function login(){
-window.location=
-`https://discord.com/oauth2/authorize?client_id=${1474836154225524787}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT)}&scope=identify`;
-}
+  const url =
+    `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}` +
+    `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}` +
+    `&response_type=token&scope=identify`;
 
-if(window.location.hash){
-const params=new URLSearchParams(window.location.hash.slice(1));
-const token=params.get("access_token");
-
-if(token){
-fetch("https://discord.com/api/users/@me",{
-headers:{authorization:`Bearer ${token}`}
-})
-.then(r=>r.json())
-.then(data=>{
-localStorage.setItem("discordUser",JSON.stringify(data));
-window.location="dashboard.html";
-});
-}
+  window.location.href = url;
 }
