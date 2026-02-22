4b590cf982351850a6ac26e8445dce62405eaffa
kasimbelim28-netizen <kasimbelim28@gmail.com>
2026-02-22 11:44:15 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,23 @@
+const CLIENT_ID="1474836154225524787";
+const REDIRECT=window.location.origin;
+
+function login(){
+window.location=
+`https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT)}&scope=identify`;
+}
+
+if(window.location.hash){
+const params=new URLSearchParams(window.location.hash.slice(1));
+const token=params.get("access_token");
+
+if(token){
+fetch("https://discord.com/api/users/@me",{
+headers:{authorization:`Bearer ${token}`}
+})
+.then(r=>r.json())
+.then(data=>{
+localStorage.setItem("discordUser",JSON.stringify(data));
+window.location="dashboard.html";
+});
+}
+}
