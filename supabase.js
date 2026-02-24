272a438fbc3e1e76b05391276640b1f7cc4a9890
kasimbelim28-netizen <kasimbelim28@gmail.com>
2026-02-24 17:34:06 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,11 @@
 const SUPABASE_URL="https://xbwgottrnhubmpxhvkrk.supabase.co";
 const SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhid2dvdHRybmh1Ym1weGh2a3JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MzI2NzMsImV4cCI6MjA4NzMwODY3M30.EBKwVxdzCtVv9P_S7-IFs1V-NeXRwhuFAL6FUNBEsvk";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+if(!window.supabase){
+console.error("Supabase library missing");
+}
+
+const supabase = window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY);
 
 async function checkLock(){
 
@@ -14,17 +18,17 @@ let { data, error } = await supabase
 .single();
 
 if(error){
-console.log("Lock check error:", error.message);
+console.log("Lock check error:",error.message);
 return;
 }
 
-if(data && data.locked === true){
-document.body.innerHTML =
+if(data && data.locked===true){
+document.body.innerHTML=
 "<h1 style='text-align:center;margin-top:50px'>Website Locked By Admin</h1>";
 }
 
 }catch(e){
-console.log("Unexpected error:", e);
+console.log("Unexpected error:",e);
 }
 
 }
